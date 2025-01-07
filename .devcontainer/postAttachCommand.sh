c55ce906509f439579ed83555208c5ae934ea886
shizukanaoki <shizukanaoki14@gmail.com>
2025-01-07 15:58:17 +0900

    make available in dev container

--- .devcontainer/postAttachCommand.sh
+++ .devcontainer/postAttachCommand.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-gh codespace ports visibility 3000:public -c $CODESPACE_NAME
-gh codespace ports visibility 8000:public -c $CODESPACE_NAME
+if [ -n "$CODESPACES" ]; then
+  gh codespace ports visibility 3000:public -c $CODESPACE_NAME
+  gh codespace ports visibility 8000:public -c $CODESPACE_NAME
+fi
--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 sudo git config --system core.editor 'code --wait'
-cd ./environment && docker-compose up -d
+docker compose -f $HOME/environment/docker-compose.yml up -d
